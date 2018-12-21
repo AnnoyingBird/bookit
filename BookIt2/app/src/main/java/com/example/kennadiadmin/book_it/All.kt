6bb5352da32afccf588b4da6e44f9f47718d9cd6
K.A <kennadi@my.prideprepschool.org>
2018-12-20 17:07:50 -0800

    Add pages

---
+++ BookIt2/app/src/main/java/com/example/kennadiadmin/book_it/All.kt
@@ -0,0 +1,12 @@
+package com.example.kennadiadmin.book_it
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class All : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all)
+    }
+}
